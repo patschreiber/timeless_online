d2760463be7502f50e8e650766d1fcf9d1b40802
Patrick Schreiber <ptos382@gmail.com>
2014-10-09 05:06:58 -0500

    Removed building wing migration

--- db/migrate/20140904212628_create_building_wings.rb
+++
@@ -1,9 +0,0 @@
-class CreateBuildingWings < ActiveRecord::Migration
-  def change
-    create_table :building_wings do |t|
-      t.string :name
-      t.integer :cost
-      t.string :flavor_text
-    end
-  end
-end
