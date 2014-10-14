b22ff79a01eeb72bda907cbb7731b0c5651d4e11
patricks <patrick@nori.nu>
2014-10-14 13:17:35 -0500

    fixed registration bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,18 +2,20 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     super
     unless resource.admin?
-      @user_stats = UserStat.new(
-        :id => resource.id,
-        :user_id => resource.id,      
-        :level => 1,
-        :current_gold => 0,
-        :hp => 100,
-        :mp => 10,
-        :base_attack => 3,
-        :base_defense => 0,
-        :speed => 50  
-      )
-      @user_stats.save!
+      if resource.save
+        @user_stats = UserStat.new(
+          :id => resource.id,
+          :user_id => resource.id,      
+          :level => 1,
+          :current_gold => 0,
+          :hp => 100,
+          :mp => 10,
+          :base_attack => 3,
+          :base_defense => 0,
+          :speed => 50  
+        )
+        @user_stats.save!
+      end
     end
   end
 
