1f6f610fdb2e59a8b6de03c9400ddd29a129b17b
Patrick Schreiber <ptos382@gmail.com>
2015-01-04 02:01:42 -0600

    Lots of inventory equipping work. Items are now equipped or overwritten in UserEquippedItems table

--- app/assets/javascripts/inventory.js
+++ app/assets/javascripts/inventory.js
@@ -16,6 +16,17 @@ $(document).ready(function() {
       data: data,
       success: function(data) {
         console.log(data);
+
+        // Equip item
+        var equipped_item_inventory_row = $('#item-' + data.equipped_item.unique_item_id);
+
+        // Only add the one checkmark signifying the item is equipped
+        if ( !equipped_item_inventory_row.find('.equipped').hasClass("ss-check") ) {
+          equipped_item_inventory_row.find('.equipped').addClass('ss-icon ss-check text-center text-success');
+        }
+        equipped_item_inventory_row.find('.equip-item').text('Unequip');
+
+        // Unequip item
       },
       error: function() {},
       complete: function() {},
--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -14,21 +14,32 @@ class InventoryController < ApplicationController
   def equip_item
     @user = current_user
     item_to_equip_id = params['item_to_equip']
+    data = {}
 
-    # Get the base item id in order to see if the item is equippable or not
-    @item_to_equip = @user.user_inventories.find_by_unique_item_id(item_to_equip_id)
-    base_item = GeneratedItem.find_by_item_id(@item_to_equip.item_id)
+    # Get the generated unique item id in order to see if the item is equippable or not
+    @item_to_equip_in_inventory = @user.user_inventories.find_by_unique_item_id(item_to_equip_id)
+    base_item = GeneratedItem.find_by_unique_item_id(@item_to_equip_in_inventory.unique_item_id)
     can_equip = base_item.can_equip
     equip_slot = base_item.equip_slot
 
+    # Only equip the item if the item has the capacity to be equipped
     if can_equip && equip_slot != nil
-      #Check if item in slot is already equipped and unequip if necessary
-      #Equip new item
-      Rails.logger.debug can_equip
-      Rails.logger.debug equip_slot
+      @user_equipped_item_slot = UserEquippedItem.find_by_user_id_and_equip_slot(@user.id, equip_slot)
+
+      # Check if item in slot is already equipped with a different item and unequip if necessary
+      # There is already another item equipped
+      if @user_equipped_item_slot.unique_item_id.present?
+        # We're only going to return the unique_item_id because we only need to unequip the item.
+        # There is no need to send a whole item object back for the unequip
+        data['unequipped_item'] = @user_equipped_item_slot.unique_item_id
+      end
+
+      # Equip new item
+      @user_equipped_item_slot.unique_item_id = item_to_equip_id
+      @user_equipped_item_slot.save!
     end
 
-    data = @item_to_equip
+    data['equipped_item'] = base_item
     render :json => data
   end
 end
